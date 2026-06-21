f54e0661ac39bb40c711aa56b72971b6668e724a
pheonixxed <1337axel1337@gmail.com>
2026-06-21 18:13:50 +0200

    testing format location change

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -1,13 +1,5 @@
 import { FormatList } from "../sim/dex-formats";
 
 export const Formats: FormatList = [
-	{
-		section: "Rogue Lineage",
-	},
-	{
-		name: "[Gen 9] Rogue Lineage OU",
-		mod: 'roguemons',
-		ruleset: ['Standard Rogue Lineage'],
-		banlist: ['Greater-Navaran'],
-	},
+
 ];
